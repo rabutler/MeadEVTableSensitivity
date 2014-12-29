72009975bfd5401ba50c25139a50aec973195ec2
Alan Butler <rabutler@usbr.gov>
2014-12-29 12:44:06 -0700

    Initial code and report.

---
+++ code/getSensitivityFigures.R
@@ -0,0 +1,9 @@
+# creates all the figures necessary to look at the changes in the natural flow due to changes
+# in the Mead elevation-volume and elevation-area tables
+getSensitivityFigures <- function()
+{
+  # read in the data
+  oldEV <- read.table('../data/Mead_Vol_tbl_1963.txt')
+  
+  oldEV
+}
